efc31435c01bca490f0ec64cb3ed8923ad4a0e72
Lucas Jensen <60709632+ljensen505@users.noreply.github.com>
2023-08-14 10:03:44 -0700

    added ability to update musician information (#14)

--- app/routers/musicians/musicians.py
+++ app/routers/musicians/musicians.py
@@ -1,13 +1,12 @@
-from fastapi import APIRouter, HTTPException, status
-from app.models.queries import get_all, get_one
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from app.auth.auth import VerifyToken
+from app.models.queries import get_all, get_one, update_musician
 from app.models.models import Musician
 
 router = APIRouter(tags=["musicians"])
+token_auth_scheme = HTTPBearer()
 table = "Musicians"
-
-# TODO: add functionality for:
-# edit bio
-# replace headshot
 
 
 @router.get("/", response_model=dict[int, Musician])
@@ -25,3 +24,23 @@ async def musician(musician_id: int) -> Musician:
             detail="No musician with that id was found",
         )
     return Musician(**data)
+
+
+@router.put("/", response_model=Musician)
+async def update(
+    new_m: Musician,
+    token: HTTPAuthorizationCredentials = Depends(token_auth_scheme),
+) -> Musician:
+    VerifyToken(token.credentials).verify()
+
+    # TODO: add headshot router AND verify that newly associated headshot exists
+
+    m = await musician(new_m.id)
+    try:
+        m.bio = new_m.bio
+        m.headshot = new_m.headshot
+        update_musician(m)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+    return m
